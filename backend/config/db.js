f59d5a6c8bf8b1290081541fd7f4b0555b63360f
nguyenthaonhi4322 <nguyenthaonhi4322@gmail.com>
2025-11-24 22:57:30 +0700

    Update db.js to connect to online database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,12 @@ const bcrypt = require('bcryptjs');
 
 // Cấu hình Sequelize với MySQL online
 const sequelize = new Sequelize(
-  process.env.DB_NAME,      // tên database
-  process.env.DB_USER,      // username
-  process.env.DB_PASSWORD,  // password
+  process.env.DB_NAME,        // tên database
+  process.env.DB_USER,        // username
+  process.env.DB_PASSWORD,    // password
   {
-    host: process.env.DB_HOST, // host MySQL online
+    host: process.env.DB_HOST,    // host MySQL Railway
+    port: process.env.DB_PORT,    // ✔️ thêm port
     dialect: 'mysql',
     logging: false
   }
@@ -15,7 +16,7 @@ const sequelize = new Sequelize(
 
 // Import các model
 const User = require('../models/User')(sequelize, DataTypes, bcrypt);
-const Writing = require('../models/Writingsave')(sequelize, DataTypes); 
+const Writing = require('../models/Writingsave')(sequelize, DataTypes);
 
 const connectDB = async () => {
     try {
@@ -28,4 +29,5 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
+
 module.exports = { sequelize, connectDB, User, Writing };
